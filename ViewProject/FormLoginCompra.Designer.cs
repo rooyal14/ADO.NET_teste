2b45456a7e50a4ba445be66cec3b9a53351e1a5d
wamaral1 <w.amaral@estudante.ifro.edu.br>
2023-06-19 02:33:46 -0400

    Livraria Visual - Login para Compras

--- ViewProject/FormLoginCompra.Designer.cs
+++ ViewProject/FormLoginCompra.Designer.cs
@@ -49,23 +49,24 @@ namespace ViewProject
             // 
             this.tableLayoutPanel3.ColumnCount = 1;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 13F));
-            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 13F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 24F));
             this.tableLayoutPanel3.Controls.Add(this.btnRecuperarSenha, 0, 0);
-            this.tableLayoutPanel3.Location = new System.Drawing.Point(32, 358);
-            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(25, 549);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 1;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(962, 80);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1314, 148);
             this.tableLayoutPanel3.TabIndex = 25;
             // 
             // btnRecuperarSenha
             // 
             this.btnRecuperarSenha.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold);
-            this.btnRecuperarSenha.Location = new System.Drawing.Point(3, 3);
+            this.btnRecuperarSenha.Location = new System.Drawing.Point(6, 6);
+            this.btnRecuperarSenha.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnRecuperarSenha.Name = "btnRecuperarSenha";
-            this.btnRecuperarSenha.Size = new System.Drawing.Size(952, 71);
+            this.btnRecuperarSenha.Size = new System.Drawing.Size(1302, 131);
             this.btnRecuperarSenha.TabIndex = 13;
             this.btnRecuperarSenha.Text = "Recuperar Senha";
             this.btnRecuperarSenha.UseVisualStyleBackColor = true;
@@ -75,9 +76,10 @@ namespace ViewProject
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Tahoma", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(21, 25);
+            this.label6.Location = new System.Drawing.Point(5, 9);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(609, 46);
+            this.label6.Size = new System.Drawing.Size(1063, 80);
             this.label6.TabIndex = 24;
             this.label6.Text = "Livraria Visual - Faça seu Login";
             // 
@@ -89,20 +91,21 @@ namespace ViewProject
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel2.Controls.Add(this.btnCadastro, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.btnEntrar, 2, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(29, 262);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(19, 372);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(962, 77);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1314, 142);
             this.tableLayoutPanel2.TabIndex = 23;
             // 
             // btnCadastro
             // 
             this.btnCadastro.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold);
-            this.btnCadastro.Location = new System.Drawing.Point(3, 3);
+            this.btnCadastro.Location = new System.Drawing.Point(6, 6);
+            this.btnCadastro.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnCadastro.Name = "btnCadastro";
-            this.btnCadastro.Size = new System.Drawing.Size(314, 71);
+            this.btnCadastro.Size = new System.Drawing.Size(426, 130);
             this.btnCadastro.TabIndex = 13;
             this.btnCadastro.Text = "Cadastre-se";
             this.btnCadastro.UseVisualStyleBackColor = true;
@@ -111,9 +114,10 @@ namespace ViewProject
             // btnEntrar
             // 
             this.btnEntrar.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold);
-            this.btnEntrar.Location = new System.Drawing.Point(643, 3);
+            this.btnEntrar.Location = new System.Drawing.Point(882, 6);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(315, 71);
+            this.btnEntrar.Size = new System.Drawing.Size(426, 130);
             this.btnEntrar.TabIndex = 16;
             this.btnEntrar.Text = "Login";
             this.btnEntrar.UseVisualStyleBackColor = true;
@@ -128,66 +132,74 @@ namespace ViewProject
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.fmEmail, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(29, 102);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 151);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.66667F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(962, 118);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1314, 181);
             this.tableLayoutPanel1.TabIndex = 22;
             // 
             // fmSenha
             // 
             this.fmSenha.Font = new System.Drawing.Font("Tahoma", 24F);
-            this.fmSenha.Location = new System.Drawing.Point(315, 62);
+            this.fmSenha.Location = new System.Drawing.Point(432, 96);
+            this.fmSenha.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.fmSenha.Name = "fmSenha";
             this.fmSenha.PasswordChar = '*';
-            this.fmSenha.Size = new System.Drawing.Size(644, 46);
+            this.fmSenha.Size = new System.Drawing.Size(876, 75);
             this.fmSenha.TabIndex = 11;
-            this.fmSenha.Text = "123";
             this.fmSenha.UseSystemPasswordChar = true;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Tahoma", 24F);
-            this.label2.Location = new System.Drawing.Point(3, 59);
+            this.label2.Location = new System.Drawing.Point(6, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(116, 39);
+            this.label2.Size = new System.Drawing.Size(200, 68);
             this.label2.TabIndex = 10;
             this.label2.Text = "Senha:";
             // 
             // fmEmail
             // 
             this.fmEmail.Font = new System.Drawing.Font("Tahoma", 24F);
-            this.fmEmail.Location = new System.Drawing.Point(315, 3);
+            this.fmEmail.Location = new System.Drawing.Point(432, 6);
+            this.fmEmail.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.fmEmail.Name = "fmEmail";
-            this.fmEmail.Size = new System.Drawing.Size(644, 46);
+            this.fmEmail.Size = new System.Drawing.Size(876, 75);
             this.fmEmail.TabIndex = 9;
-            this.fmEmail.Text = "admin";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Tahoma", 24F);
-            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Location = new System.Drawing.Point(6, 0);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(116, 39);
+            this.label4.Size = new System.Drawing.Size(202, 68);
             this.label4.TabIndex = 8;
             this.label4.Text = "E-mail:";
             // 
             // FormLoginCompra
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1530, 725);
+            this.ClientSize = new System.Drawing.Size(1443, 767);
             this.Controls.Add(this.tableLayoutPanel3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FormLoginCompra";
-            this.Text = "Livraria Visual - Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Livraria Visual - Login para Compras";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
